14995fdf9945faba35ed03941b735612d3d4a869
detoo <daily.detoo@gmail.com>
2016-04-02 12:43:58 -0700

    add more tests for the polyfill Promise.

--- src/tests/promise.spec.js
+++ src/tests/promise.spec.js
@@ -1,5 +1,23 @@
+import {expect} from 'chai';
+
 describe('Browser', function() {
   it('has defined "Promise"', function() {
-    console.log(Promise);
+    expect(Promise).to.be.an('function');
+  });
+
+  it('"Promise" can resolve', function() {
+    const value = 123;
+    Promise.resolve(value)
+      .then(function (res) {
+        expect(res).to.be.equal(value);
+      });
+  });
+
+  it('"Promise" can reject', function() {
+    const value = 123;
+    Promise.reject(value)
+      .catch(function (res) {
+        expect(res).to.be.equal(value);
+      });
   });
 });
